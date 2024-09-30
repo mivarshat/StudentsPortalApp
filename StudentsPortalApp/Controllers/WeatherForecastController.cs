f572eb81daafa5ea2895bafbdbfd224a72e3f7be
himanshu narmada <himansshunarmada14@gmail.com>
2024-09-30 18:04:24 +0530

    testing

--- StudentsPortalApp/Controllers/WeatherForecastController.cs
+++ StudentsPortalApp/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace StudentsPortalApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //testing
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
