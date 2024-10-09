5bfc30d50782b96a67e207002f82b01fff6112b6
MiVarsha <Varsha.Sonawane@dominos.co.uk>
2024-10-09 17:19:00 +0530

    Added Edit Endpoints

--- StudentsPortalApp/Controllers/StudentsController.cs
+++ StudentsPortalApp/Controllers/StudentsController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
 using StudentsPortalApp.Services;
 using StudentsPortalApp.StudentModel;
 
 namespace StudentsPortalApp.Controllers
-{   
+{
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class StudentsController : Controller
@@ -16,14 +17,15 @@ namespace StudentsPortalApp.Controllers
             _studentService = studentService;
             _logger = logger;
         }
+
         // GET: api/students
         [HttpGet]
         [Route("student")]
         [ApiVersion("1.0")]
-        public async Task<IActionResult> GetStudentsRecord()
+        public async Task<IActionResult> GetStudentsRecord(bool active)
         {
             _logger.LogInformation("Accessing Student's Record Process started");
-            var studentsRecords = await _studentService.GetStudentRecords();
+            var studentsRecords = await _studentService.GetStudentRecords(active);
             return Ok(studentsRecords);
         }
 
@@ -34,9 +36,14 @@ namespace StudentsPortalApp.Controllers
         public async Task<IActionResult> GetStudentRecordByRollNo(int rollNo)
         {
             _logger.LogInformation($"Accessing Student's Record for roll no {rollNo} Process started");
-            var studentsRecords = await _studentService.GetStudentRecords(rollNo);
-            return Ok(studentsRecords);
+            var (statusCode, result) = await _studentService.GetStudentRecords(rollNo);
+            if (statusCode == 200)
+            {
+                return Ok(result);
+            }
+            return NotFound("Student not found");
         }
+
         // GET: api/students
         [HttpGet]
         [Route("student/personal")]
@@ -55,8 +62,12 @@ namespace StudentsPortalApp.Controllers
         public async Task<IActionResult> GetStudentPersonalDetailsByRollNo(int rollNo)
         {
             _logger.LogInformation($"Accessing Student's record for roll no {rollNo} Process started");
-            var studentsRecords = await _studentService.GetStudentPersonalDetails(rollNo);
-            return Ok(studentsRecords);
+            var (statusCode, result) = await _studentService.GetStudentPersonalDetails(rollNo);
+            if (statusCode == 200)
+            {
+                return Ok(result);
+            }
+            return NotFound("Student not found");
         }
 
         // GET: api/students
@@ -77,17 +88,67 @@ namespace StudentsPortalApp.Controllers
         public async Task<IActionResult> GetStudentCurriculamDetailsById(int rollNo)
         {
             _logger.LogInformation($"Accessing Student's Curriculam Details for roll no {rollNo} Process started");
-            var studentsRecords = await _studentService.GetStudentCurriculamDetails(rollNo);
-            return Ok(studentsRecords);
+            var (statusCode, result) = await _studentService.GetStudentCurriculamDetails(rollNo);
+            if (statusCode == 200)
+            {
+                return Ok(result);
+            }
+            return NotFound("Student not found");
         }
 
         [HttpPost]
         [ApiVersion("1.0")]
-        public async Task<IActionResult> AddStudentPersonalDetails([FromBody] StudentPersonalDetails studentPersonalDetails)
+        public async Task<ActionResult<string>> AddStudentPersonalDetails([FromBody] StudentPersonalDetails studentPersonalDetails)
         {
             _logger.LogInformation("Adding Student's record Process started");
-            var studentsRecords = await _studentService.AddStudent(studentPersonalDetails);
-            return Ok(studentsRecords);
+            var response = await _studentService.AddStudent(studentPersonalDetails);
+            if(response.Item1 == 201)
+            {
+                return Ok("Student Added Successfully");
+            }
+            return BadRequest("Failed adding student");
+        }
+
+        [HttpPatch]
+        [Route("student/{rollNo}")]
+        [ApiVersion("1.0")]
+        public async Task<ActionResult<(int, string)>> ActivateDeactivateStudent(int rollNo,bool active)
+        {
+            _logger.LogInformation("Deactivating Student's record Process started");
+            var (statusCode, result) = await _studentService.ActivateDeactivateStudent(rollNo,active);
+            if (statusCode == 204)
+            {
+                return Ok(result);
+            }
+            return NotFound(result);
+        }
+
+        [HttpPatch]
+        [Route("student/personal/{rollNo}")]
+        [ApiVersion("1.0")]
+        public async Task<ActionResult<(int, string)>> EditStudentPersonalDetails(int rollNo, StudentPersonalDetails personalDetails)
+        {
+            _logger.LogInformation("Editing Student's record Process started");
+            var (statusCode, result) = await _studentService.EditStudentPersonalDetails(rollNo, personalDetails);
+            if (statusCode == 204)
+            {
+                return Ok(result);
+            }
+            return NotFound(result);
+        }
+
+        [HttpPatch]
+        [Route("student/curriculam/{rollNo}")]
+        [ApiVersion("1.0")]
+        public async Task<ActionResult<(int, string)>> EditStudentcurriculamDetails(int rollNo, StudentCurriculamDetails curriculamDetails)
+        {
+            _logger.LogInformation("Editing Student's record Process started");
+            var (statusCode, result) = await _studentService.EditStudentCurriculamDetails(rollNo, curriculamDetails);
+            if (statusCode == 204)
+            {
+                return Ok(result);
+            }
+            return NotFound(result);
         }
     }
 }
--- StudentsPortalApp/Services/StudentService.cs
+++ StudentsPortalApp/Services/StudentService.cs
@@ -6,22 +6,22 @@ namespace StudentsPortalApp.Services
 {
     public class StudentService : IStudentService
     {
-        private readonly StudentInformationlDBContext _studentService;
+        private readonly StudentInformationlDBContext _studentDbContext;
         private readonly ILogger<StudentService> _logger;
 
-        public StudentService(StudentInformationlDBContext studentService, ILogger<StudentService> logger)
+        public StudentService(StudentInformationlDBContext studentDbContext, ILogger<StudentService> logger)
         {
-            _studentService = studentService;
+            _studentDbContext = studentDbContext;
             _logger = logger;
         }
-       
+
         public async Task<List<StudentPersonalDetails>> GetStudentPersonalDetails()
         {
             List<StudentPersonalDetails> studentsRecords = new List<StudentPersonalDetails>();
 
             try
             {
-                studentsRecords = await _studentService.StudentPersonalDetails.ToListAsync();
+                studentsRecords = await _studentDbContext.StudentPersonalDetails.ToListAsync();
                 _logger.LogInformation("Received Student's Personal Details");
             }
             catch (Exception ex)
@@ -31,19 +31,20 @@ namespace StudentsPortalApp.Services
             return studentsRecords!;
         }
 
-        public async Task<StudentPersonalDetails> GetStudentPersonalDetails(int rollNo)
+        public async Task<(int, StudentPersonalDetails)> GetStudentPersonalDetails(int rollNo)
         {
             StudentPersonalDetails studentPersonalDetails = new StudentPersonalDetails();
             try
             {
-                studentPersonalDetails = await _studentService.StudentPersonalDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                studentPersonalDetails = await _studentDbContext.StudentPersonalDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
                 _logger.LogInformation($"Received Student's Personal Details for roll no {studentPersonalDetails!.RollNo}");
+               return(StatusCodes.Status200OK, studentPersonalDetails);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
             }
-            return studentPersonalDetails!;
+            return (StatusCodes.Status404NotFound, studentPersonalDetails!);
         }
 
         public async Task<List<StudentCurriculamDetails>> GetStudentCurriculamDetails()
@@ -52,7 +53,7 @@ namespace StudentsPortalApp.Services
 
             try
             {
-                studentCurriculamDetails = await _studentService.StudentCurriculamDetails.ToListAsync();
+                studentCurriculamDetails = await _studentDbContext.StudentCurriculamDetails.ToListAsync();
                 _logger.LogInformation("Received Student's Curriculam Details");
             }
             catch (Exception ex)
@@ -62,29 +63,30 @@ namespace StudentsPortalApp.Services
             return studentCurriculamDetails!;
         }
 
-        public async Task<StudentCurriculamDetails> GetStudentCurriculamDetails(int rollNo)
+        public async Task<(int,StudentCurriculamDetails)> GetStudentCurriculamDetails(int rollNo)
         {
             StudentCurriculamDetails studentCurriculamDetails = new StudentCurriculamDetails();
 
             try
             {
-                studentCurriculamDetails = await _studentService.StudentCurriculamDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                studentCurriculamDetails = await _studentDbContext.StudentCurriculamDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
                 _logger.LogInformation($"Received Student's Curriculam Details for roll no {rollNo}");
+                return (StatusCodes.Status200OK, studentCurriculamDetails);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
             }
-            return studentCurriculamDetails!;
-        }        
+            return (StatusCodes.Status404NotFound, studentCurriculamDetails);
+        }
 
-        public async Task<List<StudentRecords>> GetStudentRecords()
+        public async Task<List<StudentRecords>> GetStudentRecords(bool active)
         {
             List<StudentRecords> studentRecords = new List<StudentRecords>();
 
             try
             {
-                studentRecords = await _studentService.StudentRecords.ToListAsync();
+                studentRecords = await _studentDbContext.StudentRecords.Where(x => x.Active == active).ToListAsync();
                 _logger.LogInformation("Received Student's Details");
             }
             catch (Exception ex)
@@ -94,54 +96,134 @@ namespace StudentsPortalApp.Services
             return studentRecords!;
         }
 
-        public async Task<StudentRecords> GetStudentRecords(int rollNo)
+        public async Task<(int, StudentRecords)> GetStudentRecords(int rollNo)
         {
             StudentRecords studentRecords = new StudentRecords();
 
             try
             {
-                studentRecords = await _studentService.StudentRecords.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                studentRecords = await _studentDbContext.StudentRecords.FirstOrDefaultAsync(x => x.RollNo == rollNo);
                 _logger.LogInformation("Received Student's Details");
+                return(StatusCodes.Status200OK, studentRecords!);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
             }
-            return studentRecords!;
+            return (StatusCodes.Status404NotFound, studentRecords!);
         }
-        public async Task<StudentPersonalDetails> AddStudent(StudentPersonalDetails studentPersonalDetails)
+        public async Task<(int, StudentPersonalDetails)> AddStudent(StudentPersonalDetails studentPersonalDetails)
         {
-            var studentRecord = new StudentRecords
+            var result = await GetStudentRecords(studentPersonalDetails.RollNo);
+            if (result.Item2 == null)
             {
-                Id = studentPersonalDetails.Id,
-                RollNo = studentPersonalDetails.RollNo,
-                Active = true,
-                StudentName = studentPersonalDetails.Name
-            };
-            var studentCurriculamRecord = new StudentCurriculamDetails
-            {
-                Id = studentPersonalDetails.Id,
-                RollNo = studentPersonalDetails.RollNo,
-                Class = studentPersonalDetails.Class,
-            };
+                var studentRecord = new StudentRecords
+                {
+                    Id = studentPersonalDetails.Id,
+                    RollNo = studentPersonalDetails.RollNo,
+                    Active = true,
+                    StudentName = studentPersonalDetails.Name
+                };
+                var studentCurriculamRecord = new StudentCurriculamDetails
+                {
+                    Id = studentPersonalDetails.Id,
+                    RollNo = studentPersonalDetails.RollNo,
+                    Class = studentPersonalDetails.Class,
+                };
 
+                try
+                {
+                    _studentDbContext.StudentPersonalDetails.Add(studentPersonalDetails);
+                    _studentDbContext.StudentRecords.Add(studentRecord);
+                    _studentDbContext.StudentCurriculamDetails.Add(studentCurriculamRecord);
+                    await _studentDbContext.SaveChangesAsync();
+                    _logger.LogInformation("Successfully added Student's Personal Details as well as Curriculam Details");
+                    return (StatusCodes.Status201Created, studentPersonalDetails);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                }
+            }
+            else
+            {
+                _logger.LogInformation($"Student with Roll No {studentPersonalDetails.RollNo} already exists");
+            }
+            return (StatusCodes.Status404NotFound, studentPersonalDetails);
+        }
+        public async Task<(int, string)> ActivateDeactivateStudent(int rollNo, bool active)
+        {
             try
             {
-                _studentService.StudentPersonalDetails.Add(studentPersonalDetails);
-                _studentService.StudentRecords.Add(studentRecord);
-                _studentService.StudentCurriculamDetails.Add(studentCurriculamRecord);
-                await _studentService.SaveChangesAsync();
-                _logger.LogInformation("Successfully added Student's Personal Details as well as Curriculam Details");
+                var studentRecord = await _studentDbContext.StudentRecords.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                if (studentRecord != null)
+                {
+                    studentRecord.Active = active;
+                    await _studentDbContext.SaveChangesAsync();
+                    _logger.LogInformation($"Successfully updated Student's for roll no {rollNo}");
+                    return (StatusCodes.Status204NoContent, $"Successfully updated Student of roll no {rollNo}");
+                }
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
             }
-            return studentPersonalDetails;
+            return (StatusCodes.Status404NotFound, $"Updation of Student of roll no {rollNo} failed");
         }
-        public Task<StudentPersonalDetails> RemoveStudentPersonalDetails(int rollNo)
+
+        public async Task<(int, string)> EditStudentPersonalDetails(int rollNo, StudentPersonalDetails personalDetails)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existingPersonalDetails = await _studentDbContext.StudentPersonalDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                if (existingPersonalDetails != null)
+                {
+                    // existingPersonalDetails.RollNo = personalDetails.RollNo;
+                    existingPersonalDetails.Name = personalDetails.Name;
+                    existingPersonalDetails.Class = personalDetails.Class;
+                    existingPersonalDetails.Email = personalDetails.Email;
+                    existingPersonalDetails.Address = personalDetails.Address;
+                    existingPersonalDetails.Phone = personalDetails.Phone;
+                    existingPersonalDetails.Division = personalDetails.Division;
+
+                    await _studentDbContext.SaveChangesAsync();
+                    _logger.LogInformation($"Successfully updated Student's for roll no {rollNo}");
+                    return (StatusCodes.Status204NoContent, $"Successfully updated Student of roll no {rollNo}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return (StatusCodes.Status404NotFound, $"Updation of Student of roll no {rollNo} failed");
+        }
+
+        public async Task<(int, string)> EditStudentCurriculamDetails(int rollNo, StudentCurriculamDetails curriculamDetails)
+        {
+            try
+            {
+                var existingCurriculamDetails = await _studentDbContext.StudentCurriculamDetails.FirstOrDefaultAsync(x => x.RollNo == rollNo);
+                if (existingCurriculamDetails != null)
+                {
+                    existingCurriculamDetails.SSTMarks = curriculamDetails.SSTMarks;
+                    existingCurriculamDetails.MarathiMarks = curriculamDetails.MarathiMarks;
+                    existingCurriculamDetails.MathsMarks = curriculamDetails.MathsMarks;
+                    existingCurriculamDetails.HindiMarks = curriculamDetails.HindiMarks;
+                    existingCurriculamDetails.ComputerMarks= curriculamDetails.ComputerMarks;
+                    existingCurriculamDetails.ScienceMarks = curriculamDetails.ScienceMarks;
+                    existingCurriculamDetails.EnglishMarks = curriculamDetails.EnglishMarks;
+                    existingCurriculamDetails.Class= curriculamDetails.Class;
+
+                    await _studentDbContext.SaveChangesAsync();
+                    _logger.LogInformation($"Successfully updated Student's for roll no {rollNo}");
+                    return (StatusCodes.Status204NoContent, $"Successfully updated Student of roll no {rollNo}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return (StatusCodes.Status404NotFound, $"Updation of Student of roll no {rollNo} failed");
         }
     }
 }
